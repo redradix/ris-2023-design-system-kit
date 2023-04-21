7e224fa55716cf86828f784326490124333de1ce
Miguel del Mazo <migueldelmazo@gmail.com>
2023-04-21 12:43:18 +0200

    Added typography component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import figmaApi from 'figma-tokens/src/cli'
 
-import { createTheme, ThemeProvider, Button as MUIButton } from '@mui/material'
+import {
+  createTheme,
+  ThemeProvider,
+  Button as MUIButton,
+  Typography as MUITypography
+} from '@mui/material'
 import defaultTokens from './defaultTokens.json'
 import { jsx as _jsx } from 'react/jsx-runtime'
 
@@ -8,12 +13,21 @@ let tokens
 
 export function Button({ color, children }) {
   return /*#__PURE__*/ _jsx(MUIButton, {
+    variant: 'contained',
     sx: {
-      bgColor: color
+      color
     },
     children: children
   })
 }
+export function Typography({ color, variant, children }) {
+  return /*#__PURE__*/ _jsx(MUITypography, {
+    variant: variant || 'body1',
+    color,
+    children: children
+  })
+}
+
 export function System({ children }) {
   const {
     color: { primary }
@@ -24,6 +38,17 @@ export function System({ children }) {
       primary: {
         main: primary
       }
+    },
+    typography: {
+      h1: {
+        ...tokens.typography.heading,
+        fontSize: parseInt(tokens.typography.heading.fontSize)
+      },
+      body1: {
+        ...tokens.typography.body,
+        fontSize: parseInt(tokens.typography.body.fontSize)
+      }
+      // body: tokens.typography.body
     }
   })
   return /*#__PURE__*/ _jsx(ThemeProvider, {
@@ -31,17 +56,19 @@ export function System({ children }) {
     children: children
   })
 }
-export async function config(data) {
-  // get local storage tokens
-  const localStorageTokens = localStorage.getItem('radsFigmaTokens')
-  if (localStorageTokens) {
-    tokens = JSON.parse(localStorageTokens)
-  } else {
-    tokens = defaultTokens
-  }
-  // get figma tokens
-  const figmaTokens = await figmaApi(data)
+
+export async function config(configData) {
+  const figmaTokens = await figmaApi(configData)
+
   if (figmaTokens?.isReadyForDeploy?.isReady) {
+    tokens = figmaTokens
     localStorage.setItem('radsFigmaTokens', JSON.stringify(figmaTokens))
+  } else {
+    const localStorageTokens = localStorage.getItem('radsFigmaTokens')
+    if (!localStorageTokens) {
+      tokens = defaultTokens
+    } else {
+      tokens = JSON.parse(localStorageTokens)
+    }
   }
 }
